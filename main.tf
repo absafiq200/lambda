0b3874422ae0fcebbbccf13ccffc73fd83ecbb83
shafeeq.bari <shafeeq.bari@dish.com>
2022-09-27 17:07:51 -0600

    initial

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@
 data "archive_file" "zip_the_python_code" {
   type        = "zip"
   source_dir  = "${path.module}/python/"
-  output_path = "${path.module}/python/hello-python.zip"
+  output_path = "${path.module}/python/hello-python_1.zip"
 }
 
 resource "aws_lambda_function" "terraform_lambda_func" {
-  filename      = "${path.module}/python/hello-python.zip"
+  filename      = "${path.module}/python/hello-python_1.zip"
   function_name = var.ftn_name
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.lambda_handler"
