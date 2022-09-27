c778cbdcf1f2f1a4a120b20a0e59a650f4b91825
shafeeq.bari <shafeeq.bari@dish.com>
2022-09-27 15:52:11 -0600

    initial

--- main.tf
+++ main.tf
@@ -12,5 +12,5 @@ resource "aws_lambda_function" "terraform_lambda_func" {
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
-  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  depends_on    = [aws_cloudwatch_log_group.log_group, aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
