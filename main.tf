42220d5035fb9bf57995cbc128f14d47cd3ed4da
shafeeq.bari <shafeeq.bari@dish.com>
2022-09-27 15:04:01 -0600

    initial

--- main.tf
+++ main.tf
@@ -1,15 +1,1 @@
 
-data "archive_file" "zip_the_python_code" {
-  type        = "zip"
-  source_dir  = "${path.module}/python/"
-  output_path = "${path.module}/python/hello-python.zip"
-}
-
-resource "aws_lambda_function" "terraform_lambda_func" {
-  filename      = "${path.module}/python/hello-python.zip"
-  function_name = "wms_Lambda_Function"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "index.lambda_handler"
-  runtime       = "python3.8"
-  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
-}
--- role.tf
+++ role.tf
@@ -1,7 +1,6 @@
 resource "aws_iam_role" "lambda_role" {
-  name               = "wms_lambda_role"
-  assume_role_policy = <<EOF
-  {
+  name = "wms_lambda_role"
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
@@ -13,8 +12,7 @@ resource "aws_iam_role" "lambda_role" {
         }
       },
     ]
-  }
-  EOF
+  })
 
   tags = {
     "Name" = "wms"
@@ -28,8 +26,7 @@ resource "aws_iam_policy" "iam_policy_for_lambda" {
   name        = "aws_iam_policy_for_terraform_aws_lambda_role"
   path        = "/"
   description = "AWS IAM Policy for managing aws lambda role"
-  policy      = <<EOF
-  {
+  policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
       {
@@ -42,8 +39,7 @@ resource "aws_iam_policy" "iam_policy_for_lambda" {
         "Effect" : "Allow"
       }
     ]
-  }
-  EOF
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
@@ -51,4 +47,19 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
 
+}
+
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/python/"
+  output_path = "${path.module}/python/hello-python.zip"
+}
+
+resource "aws_lambda_function" "terraform_lambda_func" {
+  filename      = "${path.module}/python/hello-python.zip"
+  function_name = "wms_Lambda_Function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.8"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
