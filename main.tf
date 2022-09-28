e4c87f70d3635d9c0a2cecee17e22eb8d1026678
shafeeq.bari <shafeeq.bari@dish.com>
2022-09-27 18:37:35 -0600

    initial

--- main.tf
+++ main.tf
@@ -13,4 +13,5 @@ resource "aws_lambda_function" "terraform_lambda_func" {
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
   depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role, aws_cloudwatch_log_group.lg]
+  publish       = true
 }
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "lambda_version" {
+  value = aws_lambda_function.terraform_lambda_func.version
+}
